66abdbe248e61186584295969e79415b5701f13d
lauracastrovenegas <61126997+lauracastrovenegas@users.noreply.github.com>
2021-11-30 21:27:25 -0500

    Set up basic file structure for app

---
+++ src/containers/home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const PageWrapper = styled.div`
+
+`;
+
+const Home = () => {
+  return (
+      <PageWrapper>
+          This is the Home Page
+      </PageWrapper>
+  );
+}
+
+export default Home;
