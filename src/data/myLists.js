11f6f6c368779aa8dc6211a9a516e17c8c4ac3f9
lauracastrovenegas <61126997+lauracastrovenegas@users.noreply.github.com>
2021-12-06 00:59:07 -0500

    added more data for entries

---
+++ src/data/myLists.js
@@ -0,0 +1,70 @@
+export const myLists = [
+    {
+        id: "1",
+        title: "List #1",
+        category: "Restaurants",
+        date_created: "December 3, 2021",
+        entries: [
+            {
+                id: "1",
+                title: "Chipotle",
+                location: "123 Riverside Dr",
+                rating: "happy",
+                notes: "blah blah blah",
+                date_created: "December 3, 2021"
+            },
+            {
+                id: "2",
+                title: "Another Chipotle",
+                location: "123 Riverside Dr",
+                rating: "happy",
+                notes: "blah blah blah",
+                date_created: "December 3, 2021"
+            }
+        ]
+    },
+    {
+        id: "2",
+        title: "List #2",
+        category: "Books",
+        date_created: "December 3, 2021",
+        entries: [
+            {
+                id: "1",
+                title: "Harry Potter",
+                rating: "happy",
+                notes: "blah blah blah",
+                date_created: "December 3, 2021"
+            },
+            {
+                id: "2",
+                title: "Another Harry Potter",
+                rating: "happy",
+                notes: "blah blah blah",
+                date_created: "December 3, 2021"
+            }
+        ]
+    },
+    {
+        id: "3",
+        title: "List #3",
+        category: "Recipes",
+        date_created: "December 3, 2021",
+        entries: [
+            {
+                id: "1",
+                title: "Tres Leches",
+                rating: "happy",
+                notes: "blah blah blah",
+                date_created: "December 3, 2021"
+            },
+            {
+                id: "2",
+                title: "Another Cake",
+                rating: "happy",
+                notes: "blah blah blah",
+                date_created: "December 3, 2021"
+            }
+        ]
+    },
+];
