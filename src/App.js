22942979be8a6f473ae107e5ad712973639472ce
lauracastrovenegas <61126997+lauracastrovenegas@users.noreply.github.com>
2021-12-05 22:57:26 -0500

    Added theme and added new routes to app.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import theme from "./theme";
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import Home from './containers/home';
+import MyLists from './containers/myLists';
+import DiscoverPage from './containers/discover';
+import { NavBar } from './components/navbar';
 
 const App = () => {
   return (
+    <ThemeProvider theme={theme}>
       <Router>
         <Routes>
-            <Route path='/' element={<Home/>} />
+            <Route path='/' element={<DiscoverPage/>} />
+            <Route path='/my-lists/' element={<MyLists/>} />
         </Routes>
+        <NavBar/>
       </Router>
+    </ThemeProvider>
   );
 }
 
---
+++ src/theme.js
@@ -0,0 +1,16 @@
+const theme = {
+  colors: {
+    blue: '#78C0F5',
+    turquoise: '#42B7CB',
+    red: '#EC6C52',
+    darkRed: '#c63416',
+    black: '#000000',
+    white: '#F4F6F8',
+    gray: '#9A9A9A',
+    lightGray: '#E7E7E7',
+    checkboxGray: '#696969',
+    fullWhite: '#ffffff',
+  },
+};
+
+export default theme;
