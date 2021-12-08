5990cf3ebdffe21dbc9772ab9334e01d56bf3e4e
lauracastrovenegas <61126997+lauracastrovenegas@users.noreply.github.com>
2021-12-08 16:45:46 -0500

    updated list card styling to include images

--- src/components/listCard.js
+++ src/components/listCard.js
@@ -12,24 +12,38 @@ const CardWrapper = styled.a`
 `;
 
 const Card = styled.div`
-    padding: 0.5rem;
     display: flex;
     flex-direction: column;
     background-color: ${(props) => props.theme.colors.fullWhite};
-    border-radius: 7px;
+    border-radius: 15px;
     box-shadow: 2px 2px 20px rgba(0, 0, 0, 0.10);
     border: 0px;
+    height: 7rem;
+
+    img {
+        border-radius: 15px;
+        object-fit: cover;
+        height:100%;
+    }
 `;
 
-export const ListCard = ({id, category, title, date_created, isPinned}) => {
+const Name = styled.a`
+    white-space: nowrap;
+    overflow: hidden;
+    display: block;
+    text-overflow: ellipsis;
+    padding: 0.5rem 0rem;
+    font-size: 1rem;
+    font-weight: 600;
+`;
+
+export const ListCard = ({id, category, title, date_created, isPinned, imageURL, isPublic}) => {
     return (
         <CardWrapper href={`/list/${id}`}>
             <Card>
-                <div>{title}</div>
-                <div>{category}</div>
-                <div>{date_created}</div>
-                <div>{(isPinned ? "pinned":"not pinned")}</div>
+                <img src={imageURL}></img>
             </Card>
+            <Name>{title}</Name>
         </CardWrapper>
     );
 }
