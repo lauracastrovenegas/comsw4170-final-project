2d7cc471fea4c7f41bf785e2a50ab36c8601bcc8
lauracastrovenegas <61126997+lauracastrovenegas@users.noreply.github.com>
2021-12-08 16:46:21 -0500

    created sort icon - temp

---
+++ src/components/sortIcon.js
@@ -0,0 +1,26 @@
+/* Reaction Icon */
+
+import React, { useEffect } from "react";
+import styled from "styled-components/macro";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLongArrowAltUp } from '@fortawesome/free-solid-svg-icons'
+import { faLongArrowAltDown } from '@fortawesome/free-solid-svg-icons'
+
+const IconWrapper = styled.div`
+    width: 1rem;
+    svg {
+        font-size: 1.5rem;
+        color: ${(props) => props.theme.colors.gray};
+    }
+    width: fit-content;
+    margin: auto 0.25rem;
+`;
+
+export const SortIcon = ({}) => {
+    return (
+        <IconWrapper>
+            <FontAwesomeIcon icon={faLongArrowAltUp}/>
+            <FontAwesomeIcon icon={faLongArrowAltDown}/>
+        </IconWrapper>
+    );
+}
